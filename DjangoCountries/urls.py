9ae16231f9d761582b34f120192fffc3d0ee0ff7
Belutkina Elena <belenka@rambler.ru>
2025-05-09 15:48:01 +0300

    Third and fourth task - country name hyperlinks and person page

--- DjangoCountries/urls.py
+++ DjangoCountries/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path
-from MyApp.views import main_page, countries_list_view
-
+from django.urls import path, re_path
+from MyApp.views import main_page, countries_list_view, country_detail_view
 
 urlpatterns = [
-    path('', main_page, name='main-page'), 
+    path('', main_page, name='main-page'),
     
-    path('countries-list/', countries_list_view, name='countries-list'),  
- 
+    path('countries-list/', countries_list_view, name='countries-list'),
+    re_path(r'^(?P<country>.+)/$', country_detail_view, name='country-detail'),
 ]
--- MyApp/views.py
+++ MyApp/views.py
@@ -2,8 +2,33 @@ from django.shortcuts import render
 
 countries = ["Россия", "США", "Великобритания", "Китай", "Франция", "Германия", "Латвия", "Испания", "Япония", "Турция", "Польша", "Италия"]
 
+languages_by_country = {
+    "Россия": ["Русский"],
+    "США": ["Английский"],
+    "Великобритания": ["Английский"],
+    "Китай": ["Китайский"],
+    "Франция": ["Французский"],
+    "Германия": ["Немецкий"],
+    "Латвия": ["Латышский"],
+    "Испания": ["Испанский"],
+    "Япония": ["Японский"],
+    "Турция": ["Турецкий"],
+    "Польша": ["Польский"],
+    "Италия": ["Итальянский"]
+}
+
+
+
 def main_page(request):
     return render(request, 'main_page.html', {})
 
 def countries_list_view(request):
     return render(request, 'countries_list.html', {"countries": countries})
+
+def country_detail_view(request, country):
+    languages = languages_by_country.get(country)
+    if not languages:
+        return render(request, 'country_not_found.html')
+    context = {'country': country, 'languages': languages}
+    return render(request, 'country_detail.html', context)
+
