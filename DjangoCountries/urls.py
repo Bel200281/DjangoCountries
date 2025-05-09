46f561be14c2ad5f8cf3690a7ab683350fffec2c
Belutkina Elena <belenka@rambler.ru>
2025-05-09 18:06:35 +0300

    Fifth task at the top of the page list of countries alphavit

--- DjangoCountries/urls.py
+++ DjangoCountries/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, re_path
-from MyApp.views import main_page, countries_list_view, country_detail_view
+from MyApp.views import main_page, countries_list_view, filtered_countries_view, country_detail_view
 
 urlpatterns = [
     path('', main_page, name='main-page'),
-    
+
     path('countries-list/', countries_list_view, name='countries-list'),
-    re_path(r'^(?P<country>.+)/$', country_detail_view, name='country-detail'),
+
+    path('<str:country>/', country_detail_view, name='country-detail'), 
+
+    path('countries/<str:letter>/', filtered_countries_view, name='filtered-countries'),
 ]
--- MyApp/views.py
+++ MyApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
 
 countries = ["Россия", "США", "Великобритания", "Китай", "Франция", "Германия", "Латвия", "Испания", "Япония", "Турция", "Польша", "Италия"]
 
@@ -17,18 +17,24 @@ languages_by_country = {
     "Италия": ["Итальянский"]
 }
 
-
-
 def main_page(request):
     return render(request, 'main_page.html', {})
 
 def countries_list_view(request):
-    return render(request, 'countries_list.html', {"countries": countries})
+    alphabet = sorted(set(c[0].upper() for c in countries))
+    return render(request, 'countries_list.html', {"alphabet": alphabet, "countries": countries})
 
 def country_detail_view(request, country):
-    languages = languages_by_country.get(country)
-    if not languages:
-        return render(request, 'country_not_found.html')
-    context = {'country': country, 'languages': languages}
-    return render(request, 'country_detail.html', context)
+    if len(country) > 1: 
+        languages = languages_by_country.get(country)
+        if not languages:
+             return render(request, 'country_not_found.html')
+           
+        context = {'country': country, 'languages': languages}
+        return render(request, 'country_detail.html', context)
+    else:
+        return redirect(reverse('filtered-countries', args=(country,)))
 
+def filtered_countries_view(request, letter):
+    filtered_countries = [c for c in countries if c.startswith(letter)]
+    return render(request, 'filtered_countries.html', {"letter": letter, "countries": filtered_countries})
