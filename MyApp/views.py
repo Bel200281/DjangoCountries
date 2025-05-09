ae7c185e99b0efd902c9b6ae54428831ae91bd7e
Belutkina Elena <belenka@rambler.ru>
2025-05-09 13:31:55 +0300

    First and second task url root and numbered list

--- MyApp/views.py
+++ MyApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+countries = ["Россия", "США", "Великобритания", "Китай", "Франция", "Германия", "Латвия", "Испания", "Япония", "Турция", "Польша", "Италия"]
+
+def main_page(request):
+    return render(request, 'main_page.html', {})
+
+def countries_list_view(request):
+    return render(request, 'countries_list.html', {"countries": countries})
